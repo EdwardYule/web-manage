1b7fbd96c51061bdf75560cde81aeabd16d509d9
edward <nowhyman@hotmail.com>
2023-04-30 16:16:58 +0800

    ✨ feat: 报酬管理

---
+++ src/views/table1/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="词条标题" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="词条类型" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="提交人" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="提交时间" width="200px">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.display_time }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="审核结果" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="审核人" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.author }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="审核时间" width="200px">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.display_time }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="操作">
+        <template slot-scope="scope">
+          <span class="operation" @click="pass(scope.row)">通过</span>
+          <span class="operation" @click="reject(scope.row)">拒绝</span>
+          <!-- <span @click="detail(scope.row)">详情</span> -->
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog :visible.sync="visible" title="词条审核">
+      sdlfkj
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      visible: false
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.operation{
+  color: blue;
+  cursor: pointer;
+  margin-right: 8px;
+}
+</style>
